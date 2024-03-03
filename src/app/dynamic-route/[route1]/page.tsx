11d330a80e6d4cdf46c5f7fab18af84590c99876
Rao Shahroz Ali <141501953+ishahroze@users.noreply.github.com>
2024-03-03 21:40:38 +0500

    deleted

--- src/app/dynamic-route/[route1]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function dynamicroute({params}:{params:{route1:number}}) {
-  return (
-    <div>this is dynamic route number {params.route1}</div>
-  )
-}
-
-export default dynamicroute
